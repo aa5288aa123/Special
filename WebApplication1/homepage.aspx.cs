cce0e39f8ba2d9f2bc66f192e78f70eccc50ec33
致宇 柳 <107111237@mail.oit.edu.tw>
2021-06-21 11:17:10 +0800

    主頁面

--- WebApplication1/homepage.aspx.cs
+++ WebApplication1/homepage.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication1
 {
-    public partial class home_page : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class homepage : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
